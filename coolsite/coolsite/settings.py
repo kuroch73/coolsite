21676530677c9b65cdb79f14ae01a4d28b60585d
kuroch73 <kuroch73@yandex.ru>
2024-02-12 13:26:11 +0400

    ststic added

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,8 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'coolsite.urls'
-STATICFILES_DIRS = [
-   BASE_DIR / "static",
+STATICFILES_DIRS =  [
+   os.path.join(BASE_DIR, "static"),
 ]
 
 TEMPLATES = [
