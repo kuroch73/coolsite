ae8101189e3de6f226f8fb79d85e5bb86d9fa672
kuroch73 <kuroch73@yandex.ru>
2024-02-13 20:42:36 +0400

    base done

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',include('about.urls')),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('service/', include('service.urls')),
     path('vacancy/', include('vacancy.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
