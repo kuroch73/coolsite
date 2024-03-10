f1fbc4c4197de272c11bfe823ac9f13ec7bdc194
kuroch73 <kuroch73@yandex.ru>
2024-03-10 20:12:48 +0400

    vacancys advanced

--- coolsite/vacancy/models.py
+++ coolsite/vacancy/models.py
@@ -4,3 +4,6 @@ class VacancyModel(models.Model):
     name = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
